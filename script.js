146882dcb33f56dd94c76b6241b0eebb4cf845d0
myruzu <myruzu@gmail.com>
2023-06-05 19:53:51 +0900

    Made cost analysis of the plate, added the new logo, added nav bar and changed color scheme to black and white

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 
 
-        function countChars(obj){
-            var maxLength = 120;
-            var strLength = obj.value.length;
-            var charRemain = (maxLength - strLength);
+  //       function countChars(obj){
+  //           var maxLength = 120;
+  //           var strLength = obj.value.length;
+  //           var charRemain = (maxLength - strLength);
             
             
-            if (charRemain < 0) {
+  //           if (charRemain < 0) {
                 
-                document.getElementById('charNum').innerHTML = '<span style="color:red;">You have exceeded the limit of 120 characters</span>';
-            }
+  //               document.getElementById('charNum').innerHTML = '<span style="color:red;">You have exceeded the limit of 120 characters</span>';
+  //           }
 
-            else {
-                document.getElementById('charNum').innerHTML = 'You have '+charRemain+' characters remaining';
-                }
-  }
+  //           else {
+  //               document.getElementById('charNum').innerHTML = 'You have '+charRemain+' characters remaining';
+  //               }
+  // }
 
 
     
@@ -42,34 +42,148 @@ function changePlateStyleBlackGold() {
 function changePlateStyleBlackSilver() {
   document.getElementById("output-text").style.backgroundColor = "#0D0C08";
   
-  document.getElementById("output-text").style.color = "#AFA9A4";
+  document.getElementById("output-text").style.color = "#AFA9A4"; }
 
+
+
+// Cost Calculator
+
+      function calculateCost() {
+        // Get the textarea element and the content typed by the user
+        var textarea = document.getElementById("output-text");
+        var content = textarea.value;
   
+        // Split the content into lines
+        var lines = content.split(/\r\n|\r|\n/);
+  
+        // Filter out empty lines
+        var nonEmptyLines = lines.filter(function(line) {
+          return line.trim() !== "";
+        });
+  
+        // Calculate the total cost based on the number of non-empty lines
+        var cost = nonEmptyLines.length * 3;
+  
+        // Display the total cost in a separate element
+        document.getElementById("total").textContent = "$" + cost.toFixed(2);
+  
+        // Display the line count in a separate element
+        document.getElementById("lineCount").textContent = nonEmptyLines.length;
+      }
 
+
+
+
+
+
+
+
+      // Directions for design bounce
+
+// Get the reference to the element
+const title = document.querySelector('.bounce');
+
+// Function to check if the element is in the viewport
+function isElementInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
 }
 
-// const placeholderColor = () => {
-//   let textBox = document.getElementById("output-text");
-//   let txtbxPlaceholder = window.getComputedStyle(textBox, '::placeholder');
-//   let changeColortxtbx = txtbxPlaceholder.style.color = "#AFA9A4";
-// };
+// Function to add the animation classes
+function startAnimation() {
+  title.classList.add('animated', 'fade-in');
+}
 
-//   function changeColor(placeholderColor) {
-//     let changeColortxtbx;
-//   };
+// Function to remove the animation classes
+function stopAnimation() {
+  title.classList.remove('animated', 'fade-in');
+}
 
-
-
-
-
-// var element = document.querySelector('');
-// var placeholder = window.getComputedStyle(element,'::placeholder');
-// var content = styles['color'];
-
-
+// Event listener for scroll event
+window.addEventListener('scroll', () => {
+  if (isElementInViewport(title)) {
+    startAnimation();
+    setTimeout(stopAnimation, 5000); // Stop animation after 5 seconds
+  }
+});
 
 
-// Alert for receipt number
+// Plate size adjustment
+
+function updateTextAreaSize() {
+  var widthInput = document.getElementById('widthInput');
+  var heightInput = document.getElementById('heightInput');
+  var textarea = document.getElementById('output-text');
+
+  if (widthInput.value !== '' && heightInput.value !== '') {
+    var widthInPixels = widthInput.value * 97;
+    var heightInPixels = heightInput.value * 97;
+
+    textarea.style.width = widthInPixels + 'px';
+    textarea.style.height = heightInPixels + 'px';
+    textarea.removeAttribute('readonly');
+  } else {
+    textarea.style.width = '388px';
+    textarea.style.height = '194px';
+    textarea.setAttribute('readonly', 'readonly');
+  }
+}
+
+// Update textarea size when the window is resized
+window.addEventListener('resize', updateTextAreaSize);
+
+
+
+
+// Code for generating plate material cost
+var squareInchPrice = 0.45;
+
+function updateTextAreaSize() {
+  var widthInput = document.getElementById('widthInput');
+  var heightInput = document.getElementById('heightInput');
+  var textarea = document.getElementById('output-text');
+  var squareInchesInput = document.getElementById('squareInches');
+  var totalCostInput = document.getElementById('totalCost');
+
+  var width = parseFloat(widthInput.value);
+  var height = parseFloat(heightInput.value);
+
+  if (!isNaN(width) && !isNaN(height)) {
+    var squareInches = width * height;
+    var totalCost = squareInches * squareInchPrice;
+
+    textarea.style.width = width * 97 + 'px';
+    textarea.style.height = height * 97 + 'px';
+
+    squareInchesInput.value = squareInches.toFixed(2);
+    totalCostInput.value = totalCost.toFixed(2);
+  } else {
+    textarea.style.width = '';
+    textarea.style.height = '';
+    squareInchesInput.value = '';
+    totalCostInput.value = '';
+  }
+}
+
+function resetSquareInchPrice() {
+  squareInchPrice = 0.45;
+  updateTextAreaSize();
+}
+
+
+function changeSquareInchPrice() {
+  squareInchPrice = 0.35;
+  updateTextAreaSize();
+}
+
+
+
+      
 
 function checkNumber(event) {
   var aCode = event.which ? event.which : event.keyCode;
